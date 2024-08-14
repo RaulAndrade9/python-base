df0f40e0c2448a8edec4e53600cde00927469e5c
Raul Andrade <raul.sandrade@hotmail.com>
2024-08-14 14:02:14 -0300

    modificações realizadas

--- hello.py
+++ hello.py
@@ -1,3 +1,26 @@
+"""Hello World Multi Linguas.
+
+Dependendo da língua configurada no ambiente o programa exibe a mensagem correspondente
+
+como usar: 
+Tenha a variável LANG devidamente configurada ex: export LANG=pt_BR
+
+Execução: 
+    python3 hello.py
+
+"""
+
+__version__ = "0.0.1"
+__author__ = "Raul Andrade"
+__license__ = "Unlicense "
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = "Hello World"
+
+if current_language == "pt_BR":
+    msg = "Olá Mundo!"
 
 
-print("Hello World")
+print(msg)
