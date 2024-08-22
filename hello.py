bc02a3c46eff99c4e7f107b876963216d3322771
Raul Andrade <raul.sandrade@hotmail.com>
2024-08-22 13:57:08 -0300

    resolução de exercicios utilizando cli

--- hello.py
+++ hello.py
@@ -5,18 +5,44 @@ Dependendo da língua configurada no ambiente o programa exibe a mensagem corres
 como usar: 
 Tenha a variável LANG devidamente configurada ex: export LANG=pt_BR
 
+ou informe através do CLI argument --lang
+ou o usuário terá que digitar
+
 Execução: 
     python3 hello.py
 
 """
 
-__version__ = "0.0.1"
+__version__ = "0.1.3"
 __author__ = "Raul Andrade"
 __license__ = "Unlicense "
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+arguments = {
+    "lang": None,
+    "count": 2,
+}
+
+for arg in sys.argv[1:]:
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid Option `{key}`")
+        sys.exit()
+    arguments[key] = value
+    
+
+
+
+
+current_language = arguments["lang"]
+if current_language is None:
+    current_language = os.getenv("LANG", "en_US")[:5]
+
+
 msg = {
     "en_US": "Hello World",
     "pt_BR": "Olá Mundo",
@@ -26,4 +52,4 @@ msg = {
 }
 
 
-print(msg[current_language])
+print(msg[current_language] * int(arguments["count"]))
---
+++ set_dict.py
@@ -0,0 +1,7 @@
+conjunto = {1, 2, 4, 5, 8, 9, 12, 30}
+lista = list(conjunto)
+posicao = 1
+for numero in lista:
+    print(f"A posição é: {posicao} O número é: {numero}")
+    posicao+= 1
+    
